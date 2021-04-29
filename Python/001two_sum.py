aa1a94b0200677610f8c1a9872c0cfd66f3bb7eb
YIYANG DONG <tonyyoung9838@gmail.com>
2021-04-29 09:58:05 -0700

    Create 001two_sum.py

---
+++ Python/001two_sum.py
@@ -0,0 +1,22 @@
+# Approach 1: Brute Force
+# Time: O(n^2) Space: O(1)
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        return []
+
+# Approach 2: Two-pass Hash Table
+# Time: O(n) Space: O(n)
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
