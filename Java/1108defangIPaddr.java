bfc8c5b105eb32a39900767b18f67a11dd3f1c57
YIYANG DONG <tonyyoung9838@gmail.com>
2021-04-29 19:48:13 -0700

    add 1108

---
+++ Java/1108defangIPaddr.java
@@ -0,0 +1,8 @@
+public String defangIPaddr(String address) {
+    StringBuilder sb = new StringBuilder();
+    for (char c : address.toCharArray()) {
+        sb.append(c == '.' ? "[.]" : c);
+    }
+    return sb.toString();
+    //return address.replace(".", "[.]");
+}
