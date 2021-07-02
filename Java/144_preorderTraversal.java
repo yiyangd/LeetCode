620285e2a0ddefeacc159f74cc3a4360ab48b270
YIYANG DONG <tonyyoung9838@gmail.com>
2021-07-01 17:21:26 -0700

    Update 144_preorderTraversal.java

--- Java/144_preorderTraversal.java
+++ Java/144_preorderTraversal.java
@@ -14,7 +14,9 @@
  * }
  */
 
-// Solution 1 - 
+// Solution 1 - Iteration
+// Time Complexity: O(n)
+// Space Compexity: O(n)
 public List<Integer> preorderTraversal(TreeNode root) {
     LinkedList <TreeNode> stack = new LinkedList<> ();
     LinkedList <Integer> output = new LinkedList<> ();
@@ -25,10 +27,26 @@ public List<Integer> preorderTraversal(TreeNode root) {
     while(!stack.isEmpty()){
         TreeNode node = stack.pollLast();
         output.add(node.val);
-        if(node.right!=null) stack.add(node.right);
-        if(node.left!=null) stack.add(node.left);
+        if(node.right!=null) stack.add(node.right); //先入后出
+        if(node.left!=null) stack.add(node.left);   //后入先出
     }
     return output;
 }
 
 // Solution 2 - Recursion
+// Time Complexity: O(n)
+// Space Compexity: O(n)
+public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<Integer>();
+    preorder(root, res);
+    return res;
+}
+
+public void preorder(TreeNode root, List<Integer> res) {
+    if (root == null) {
+        return;
+    }
+    res.add(root.val);
+    preorder(root.left, res);
+    preorder(root.right, res);
+}
