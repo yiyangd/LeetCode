6e84c8b7cef4ff915fe372dd3f999bc5c4dd0eb4
YIYANG DONG <tonyyoung9838@gmail.com>
2021-07-17 13:40:27 -0700

    Update 047_permuteUnique.java

--- Java/047_permuteUnique.java
+++ Java/047_permuteUnique.java
@@ -34,3 +34,42 @@ Time Complexity: $O(n · n!)$
 Space Complexity: O(n+n) = O(n)
 - O(n) for `vis` and O(n) for recursion stack
 */
+
+//Solution 2 - Backtracking with HashMap
+private List<List<Integer>> perms = new ArrayList<>();
+public List<List<Integer>> permuteUnique(int[] nums) {
+    HashMap<Integer,Integer> hm = new HashMap<>();
+    for (int i = 0; i < nums.length; i++){
+        int count = 1;
+        if (hm.containsKey(nums[i]))
+            count += hm.get(nums[i]);
+        hm.put(nums[i], count);
+    }
+    int[] uniNums = new int[hm.size()];
+    int[] counts = new int[hm.size()];
+    int k = 0;
+    for (int i = 0; i < nums.length; i++){
+        if (hm.containsKey(nums[i])) {
+            uniNums[k] = nums[i];
+            counts[k++] = hm.get(nums[i]);
+            hm.remove(nums[i]);
+        }
+    }
+    backtrack(uniNums, counts, 0, new ArrayList(), nums.length);
+    return perms;
+}
+private void backtrack(int[] uniNums, int[] counts, int idx, List<Integer> perm, int n) {
+    if (idx == n){
+        perms.add(new ArrayList<>(perm));
+        return;
+    }
+    for (int i = 0; i < uniNums.length; i++) {
+        if (counts[i] == 0) continue;
+        perm.add(uniNums[i]);
+        counts[i]--;
+        backtrack(uniNums,counts,idx+1,perm,n);
+        perm.remove(perm.size()-1);
+        counts[i]++;
+    }
+}
+
