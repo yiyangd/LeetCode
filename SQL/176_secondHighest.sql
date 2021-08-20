c182672e04d065d62e63a9aa281e82de772bbb29
YIYANG DONG <tonyyoung9838@gmail.com>
2021-08-19 22:27:40 -0700

    Create 176_secondHighest.sql

---
+++ SQL/176_secondHighest.sql
@@ -0,0 +1,16 @@
+SELECT
+    (SELECT DISTINCT Salary
+        FROM Employee
+        ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1) AS SecondHighestSalary
+;
+
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+       LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+  ;
